b310ab40219c3629b279a563c2344e67ebea7176
ernesto <ernesto@pigmalion.com.ar>
2024-08-13 16:28:42 -0300

    update

--- api/users.go
+++ api/users.go
@@ -3,20 +3,17 @@ package handler
 import (
 	"encoding/json"
 	"fmt"
+	"golangnext/models"
 	"net/http"
 )
-
-type User struct {
-	ID       int    `json:"id"`
-	Username string `json:"username"`
-}
 
 func Users(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
-	u := User{
+	u := models.User{
 		ID:       1,
 		Username: "johndoe",
+		LastName: "Ponce",
 	}
 
 	// Convert user struct to JSON string
