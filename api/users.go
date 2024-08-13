0f207f3b9637606dbd8782bb3a00f0c036993e08
ernesto <ernesto@pigmalion.com.ar>
2024-08-13 16:19:33 -0300

    update

--- api/users.go
+++ api/users.go
@@ -1,11 +1,26 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
+	"golangnext/api/models"
 	"net/http"
 )
 
 func Users(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	fmt.Fprintf(w, `{"message": "Hello, users!"}`)
+
+	u := models.User{
+		ID:       1,
+		Username: "johndoe",
+	}
+
+	// Convert user struct to JSON string
+	jsonString, err := json.Marshal(u)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprint(w, string(jsonString))
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module golangnext
+
+go 1.22.5
